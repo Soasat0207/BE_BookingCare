0d3b5bd77cf0956103d3daa8d2c03bc4a048c760
Hiep <lehuyhiep449@gmail.com>
2021-10-14 19:17:52 +0700

    update info doctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,18 +1,57 @@
 import doctorServices from '../services/doctorServices'
-let getTopDoctorHomeCode = async (req, res)=>{
+let getTopDoctorHomeCode = async (req, res) => {
     let limit = req.query.limit;
-    if(!limit) limit = 10;
+    if (!limit) limit = 10;
     try {
         let response = await doctorServices.getTopDoctorHome(+limit);
         return res.status(200).json(response);
     } catch (error) {
         console.error(error);
         return res.status(200).json({
-            errCode:-1,
+            errCode: -1,
             message: 'Error from server ...'
         })
     }
 }
-module.exports ={
-    getTopDoctorHomeCode:getTopDoctorHomeCode,
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorServices.getAllDoctors();
+        return res.status(200).json(doctors)
+    } catch (error) {
+        console.error(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server ...'
+        })
+    }
+}
+let postInfoDoctor = async (req, res) => {
+    try {
+        let response = await doctorServices.saveDetailsInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.error(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server ...'
+        })
+    }
+}
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let info = await doctorServices.getDetailDoctorById(req.query.id);
+        return res.status(200).json(info);
+    } catch (error) {
+        console.error(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server ...'
+        })
+    }
+}
+module.exports = {
+    getTopDoctorHomeCode: getTopDoctorHomeCode,
+    getAllDoctors: getAllDoctors,
+    postInfoDoctor:postInfoDoctor,
+    getDetailDoctorById:getDetailDoctorById
 }
--- src/services/doctorServices.js
+++ src/services/doctorServices.js
@@ -24,6 +24,89 @@ let getTopDoctorHome = (limit) => {
         }
     })
 }
+let getAllDoctors = () =>{
+    return new Promise(async(resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where:{roleId:'R2'},
+                attributes:{
+                    exclude:['password']
+                },
+            })
+            resolve({
+                errCode:0,
+                data:doctors,
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+let saveDetailsInfoDoctor =(data)=>{
+    return new Promise( async (resolve, reject) => {
+        try {
+            if(!data.doctorId || !data.contentHTML || !data.contentMarkdown){
+                resolve({
+                    errCode:1,
+                    errMessage:'Missiong parameter'
+                })
+            } 
+            else{
+                await db.Markdown.create({
+                    contentHTML:data.contentHTML,
+                    contentMarkdown:data.contentMarkdown,
+                    description:data.description,
+                    doctorId:data.doctorId,
+                })
+                resolve({
+                    errCode:0,
+                    errMessage:'Save Info Doctor Success'
+                })
+            }
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+let getDetailDoctorById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if(!id) {
+                resolve({
+                    errCode:1,
+                    errMessage:'Missiong parameter'
+                })
+            }
+            else{
+                let data = await db.User.findOne({
+                    where: {id:id},
+                    include:[
+                        {
+                            model:db.Markdown,
+                            attributes:['description','contentHTML','contentMarkdown'],
+                        },
+                        {model:db.Allcode,as:'positionData',attributes:['valueEn','valueVi']},
+                    ],
+                    attributes:{
+                        exclude:['password']
+                    },
+                    raw: true,
+                    nest:true,
+                })
+                resolve({
+                    errCode:0,
+                    data:data,
+                })
+                
+            }
+        } catch (error) {
+            reject(error); 
+        }
+    })
+}
 module.exports = {
-    getTopDoctorHome:getTopDoctorHome
+    getTopDoctorHome:getTopDoctorHome,
+    getAllDoctors:getAllDoctors,
+    saveDetailsInfoDoctor:saveDetailsInfoDoctor,
+    getDetailDoctorById:getDetailDoctorById,
 }
